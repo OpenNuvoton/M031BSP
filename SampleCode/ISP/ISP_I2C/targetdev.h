378258b811e5fe85c0a46d59f7a8b8f2be78ae29
“ycyang9” <ycyang9@nuvoton.com>
2019-05-09 16:02:55 +0800

    Added ISP sample for I2C and SPI
    Change-Id: I4ee35720c18e553b12d6fc5a55152a52429f8a55

---
+++ SampleCode/ISP/ISP_I2C/targetdev.h
@@ -0,0 +1,17 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x34
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+
+#include "NuMicro.h"
+#include "isp_user.h"
+#include "i2c_transfer.h"
+
+#define ReadyPin  (PB6)
+
+#define CONFIG_SIZE 8 // in bytes
---
+++ SampleCode/ISP/ISP_SPI/spi_transfer.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * @file     spi_transfer.h
+ * @brief    SPI ISP slave header file
+ * @version  1.0.0
+ *
+ * @note
+ * Copyright (C) 2017 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#ifndef __SPI_TRANS_H__
+#define __SPI_TRANS_H__
+#include <stdint.h>
+
+extern volatile uint8_t bSpiDataReady;
+extern uint32_t spi_rcvbuf[];
+
+/*-------------------------------------------------------------*/
+void SPI_Init(void);
+
+#endif  /* __SPI_TRANS_H__ */
