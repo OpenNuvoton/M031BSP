f9b1c0804e3c2ef93b4b8053dab14f8aaa0aa75c
kchuang1 <kchuang1@nuvoton.com>
2019-07-30 19:45:53 +0800

    Added ISP_RS485 sample code
    Change-Id: I3b8f64d66dc65e0b5d5eabbb7ce2fc97a1e15807

---
+++ SampleCode/ISP/ISP_RS485/main.c
@@ -0,0 +1,117 @@
+/***************************************************************************//**
+ * @file     main.c
+ * @brief    ISP tool main function
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "targetdev.h"
+#include "uart_transfer.h"
+
+#define nRTSPin                 (PA0)
+#define RECEIVE_MODE            (0)
+#define TRANSMIT_MODE           (1)
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    /* Enable HIRC clock (Internal RC 48MHz) */
+    CLK->PWRCTL |= (CLK_PWRCTL_HIRCEN_Msk | CLK_PWRCTL_HXTEN_Msk);
+
+    /* Wait for HIRC clock ready */
+    while (!(CLK->STATUS & CLK_STATUS_HIRCSTB_Msk));
+
+    /* Select HCLK clock source as HIRC and HCLK clock divider as 1 */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLKSEL_Msk)) | CLK_CLKSEL0_HCLKSEL_HIRC;
+    CLK->CLKDIV0 = (CLK->CLKDIV0 & (~CLK_CLKDIV0_HCLKDIV_Msk)) | CLK_CLKDIV0_HCLK(1);
+    /* Set PLL to Power-down mode */
+    CLK->PLLCTL |= CLK_PLLCTL_PD_Msk;
+    /* Enable UART module clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_UART1CKEN_Msk;
+    /* Select UART module clock source as HXT and UART module clock divider as 1 */
+    CLK->CLKSEL1 = (CLK->CLKSEL1 & (~CLK_CLKSEL1_UART1SEL_Msk)) | CLK_CLKSEL1_UART1SEL_HIRC;
+    CLK->CLKDIV0 = (CLK->CLKDIV0 & (~CLK_CLKDIV0_UART1DIV_Msk)) | CLK_CLKDIV0_UART1(1);
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    PA->MODE = (PA->MODE & ~(0x3ul << (0 << 1))) | (GPIO_MODE_OUTPUT << (0 << 1));
+    SYS->GPA_MFPL &= ~(SYS_GPA_MFPL_PA2MFP_Msk | SYS_GPA_MFPL_PA3MFP_Msk );
+    SYS->GPA_MFPL |= (SYS_GPA_MFPL_PA2MFP_UART1_RXD | SYS_GPA_MFPL_PA3MFP_UART1_TXD );
+
+    nRTSPin = RECEIVE_MODE;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+    /* Init UART to 115200-8n1 */
+    UART_Init();
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    FMC->ISPCTL |= (FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_APUEN_Msk);
+    g_apromSize = GetApromSize();
+    GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+    SysTick->LOAD = 300000 * CyclesPerUs;
+    SysTick->VAL   = (0x00);
+    SysTick->CTRL = SysTick->CTRL | SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+
+    while (1)
+    {
+        if ((bufhead >= 4) || (bUartDataReady == TRUE))
+        {
+            uint32_t lcmd;
+            lcmd = inpw(uart_rcvbuf);
+
+            if (lcmd == CMD_CONNECT)
+            {
+                goto _ISP;
+            }
+            else
+            {
+                bUartDataReady = FALSE;
+                bufhead = 0;
+            }
+        }
+        if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)
+        {
+            goto _APROM;
+        }
+    }
+
+_ISP:
+
+    while (1)
+    {
+        if (bUartDataReady == TRUE)
+        {
+            bUartDataReady = FALSE;
+            ParseCmd(uart_rcvbuf, 64);
+            NVIC_DisableIRQ(UART13_IRQn);
+            nRTSPin = TRANSMIT_MODE;
+            PutString();
+
+            while ((UART1->FIFOSTS & UART_FIFOSTS_TXEMPTYF_Msk) == 0);
+
+            nRTSPin = RECEIVE_MODE;
+            NVIC_EnableIRQ(UART13_IRQn);
+        }
+    }
+
+_APROM:
+    SYS->RSTSTS = (SYS_RSTSTS_PORF_Msk | SYS_RSTSTS_PINRF_Msk);
+    FMC->ISPCTL &= ~(FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while (1);
+}
---
+++ SampleCode/ISP/ISP_RS485/targetdev.h
@@ -0,0 +1,21 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+
+#include "NuMicro.h"
+#include "isp_user.h"
+
+#define DetectPin                   PB12
+
+/* rename for uart_transfer.c */
+#define UART_N                          UART0
+#define UART_N_IRQHandler       UART02_IRQHandler
+#define UART_N_IRQn                 UART02_IRQn
+
+#define CONFIG_SIZE 8 // in bytes
