ab64cbbeadc8d43ab9100a90356f1c309a74a2fe
“SPCheng0” <SPCheng0@nuvoton.com>
2019-08-14 16:54:13 +0800

    Add RTC driver & sample code
    Change-Id: Ie4e26a35fc61672b5c3852abcf837e2c89c69d4c

---
+++ SampleCode/RegBased/RTC_Time_Display/main.c
@@ -0,0 +1,158 @@
+/****************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * @brief    Demonstrate the RTC function and displays current time to the
+ *           UART console
+ *
+ *
+ * @copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Global variables                                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+
+volatile uint32_t  g_u32TICK = 0;
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* RTC Tick Handle                                                                              */
+/*---------------------------------------------------------------------------------------------------------*/
+void RTC_TickHandle(void)
+{
+    /* Get the current time */
+    printf(" Current Time:20%X/%02X/%02X %02X:%02X:%02X\n",
+           (RTC->CAL >> RTC_CAL_YEAR_Pos) & 0xFF, (RTC->CAL >> RTC_CAL_MON_Pos) & 0xFF, (RTC->CAL >> RTC_CAL_DAY_Pos) & 0xFF,
+           (RTC->TIME >> RTC_TIME_HR_Pos) & 0xFF, (RTC->TIME >> RTC_TIME_MIN_Pos) & 0xFF, (RTC->TIME >> RTC_TIME_SEC_Pos) & 0xFF);
+
+    g_u32TICK++;
+}
+
+/**
+  * @brief  RTC ISR to handle interrupt event
+  * @param  None
+  * @retval None
+  */
+void RTC_IRQHandler(void)
+{
+
+    if ( (RTC->INTEN & RTC_INTEN_TICKIEN_Msk) && (RTC->INTSTS & RTC_INTSTS_TICKIF_Msk) )        /* tick interrupt occurred */
+    {
+        RTC->INTSTS = 0x2;
+
+        RTC_TickHandle();
+    }
+
+}
+
+void Delay(uint32_t ucnt)
+{
+    volatile uint32_t i = ucnt;
+
+    while (i--);
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable LXT clock */
+    CLK->PWRCTL |= CLK_PWRCTL_LXTEN_Msk;
+
+    /* Enable HIRC clock */
+    CLK->PWRCTL |= CLK_PWRCTL_HIRCEN_Msk;
+
+    /* Waiting for HIRC clock ready */
+    while((CLK->STATUS & CLK_STATUS_HIRCSTB_Msk) != CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & ~CLK_CLKSEL0_HCLKSEL_Msk ) | CLK_CLKSEL0_HCLKSEL_HIRC ;
+
+    /* Enable UART0 clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_UART0CKEN_Msk ;
+
+    /* Switch UART0 clock source to HIRC */
+    CLK->CLKSEL1 = (CLK->CLKSEL1 & ~CLK_CLKSEL1_UART0SEL_Msk) | CLK_CLKSEL1_UART0SEL_HIRC;
+
+    /* Enable RTC clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_RTCCKEN_Msk ;
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))
+                    |(SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+
+int32_t main(void)
+{
+    SYS_Init();
+
+    /* Init UART0 to 115200-8n1 for print message */
+    /* Reset UART0 */
+    SYS->IPRST1 |=  SYS_IPRST1_UART0RST_Msk;
+    SYS->IPRST1 &= ~SYS_IPRST1_UART0RST_Msk;
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART0->BAUD = UART_BAUD_MODE2 | UART_BAUD_MODE2_DIVIDER(__HIRC, 115200);
+    UART0->LINE = UART_WORD_LEN_8 | UART_PARITY_NONE | UART_STOP_BIT_1;
+
+    /* Open RTC */
+    RTC->INIT = RTC_INIT_KEY;
+
+    if (RTC->INIT != RTC_INIT_ACTIVE_Msk)
+    {
+        RTC->INIT = RTC_INIT_KEY;
+
+        while (RTC->INIT != RTC_INIT_ACTIVE_Msk)
+        {
+        }
+    }
+    /* Time Setting */
+    RTC->CLKFMT  = RTC_CLOCK_24;
+    RTC->WEEKDAY = RTC_MONDAY;
+    RTC->CAL     = 0x00170501;         /* Date: 2017/05/01 */
+    RTC->TIME    = 0x00123000;         /* Time: 12:30:00 */
+
+
+    printf("\n RTC Time Display Test (Exit after 5 seconds)\n\n");
+
+    /* Set Tick Period */
+    RTC->TICK    = RTC_TICK_1_SEC;     /* One RTC tick is 1 second */
+
+    /* Enable RTC Tick Interrupt */
+    RTC->INTEN  = RTC_INTEN_TICKIEN_Msk;
+
+    NVIC_EnableIRQ(RTC_IRQn);
+
+    g_u32TICK = 0;
+    while(g_u32TICK < 5);
+
+    printf("\n RTC Time Display Test End !!\n");
+
+    /* Disable RTC Tick Interrupt */
+    RTC->INTEN  &= ~RTC_INTEN_TICKIEN_Msk;
+
+    NVIC_DisableIRQ(RTC_IRQn);
+
+    while(1);
+
+}
+
+
+
+
+
+
+/*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
+
+
+
---
+++ SampleCode/StdDriver/RTC_Alarm_Wakeup/main.c
@@ -0,0 +1,189 @@
+/****************************************************************************
+ * @file     main.c
+ * @version  V3.00
+ * @brief    Use RTC alarm interrupt event to wake up system.
+ * @copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Global Interface Variables Declarations                                                                 */
+/*---------------------------------------------------------------------------------------------------------*/
+extern int IsDebugFifoEmpty(void);
+volatile uint8_t g_u8IsRTCAlarmINT = 0;
+
+
+/**
+ * @brief       IRQ Handler for RTC Interrupt
+ *
+ * @param       None
+ *
+ * @return      None
+ *
+ * @details     The RTC_IRQHandler is default IRQ of RTC, declared in startup_M2351.s.
+ */
+void RTC_IRQHandler(void)
+{
+    /* To check if RTC alarm interrupt occurred */
+    if(RTC_GET_ALARM_INT_FLAG() == 1)
+    {
+        /* Clear RTC alarm interrupt flag */
+        RTC_CLEAR_ALARM_INT_FLAG();
+
+        g_u8IsRTCAlarmINT++;
+    }
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable LXT clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_LXTEN_Msk);
+
+    /* Enable HIRC clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Enable UART0 clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Enable RTC clock */
+    CLK_EnableModuleClock(RTC_MODULE);
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))
+                    |(SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+void UART0_Init()
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init UART                                                                                               */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Reset UART module */
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 Baudrate */
+    UART_Open(UART0, 115200);
+}
+
+int main(void)
+{
+    S_RTC_TIME_DATA_T sWriteRTC, sReadRTC;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART for printf */
+    UART0_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    printf("\n\nCPU @ %dHz\n", SystemCoreClock);
+    printf("+-------------------------------------+\n");
+    printf("|    RTC Alarm Wake-up Sample Code    |\n");
+    printf("+-------------------------------------+\n\n");
+
+    /* Open RTC */
+    sWriteRTC.u32Year       = 2017;
+    sWriteRTC.u32Month      = 3;
+    sWriteRTC.u32Day        = 15;
+    sWriteRTC.u32DayOfWeek  = RTC_SUNDAY;
+    sWriteRTC.u32Hour       = 23;
+    sWriteRTC.u32Minute     = 59;
+    sWriteRTC.u32Second     = 50;
+    sWriteRTC.u32TimeScale  = RTC_CLOCK_24;
+    RTC_Open(&sWriteRTC);
+
+    /* Set RTC alarm date/time */
+    sWriteRTC.u32Year       = 2017;
+    sWriteRTC.u32Month      = 3;
+    sWriteRTC.u32Day        = 15;
+    sWriteRTC.u32DayOfWeek  = RTC_SUNDAY;
+    sWriteRTC.u32Hour       = 23;
+    sWriteRTC.u32Minute     = 59;
+    sWriteRTC.u32Second     = 55;
+    RTC_SetAlarmDateAndTime(&sWriteRTC);
+
+    /* Enable RTC alarm interrupt and wake-up function will be enabled also */
+    RTC_DisableInt(RTC_INTEN_TICKIEN_Msk);
+    RTC_EnableInt(RTC_INTEN_ALMIEN_Msk);
+
+    /* Enable RTC NVIC */
+    NVIC_EnableIRQ(RTC_IRQn);
+
+    printf("# Set RTC current date/time: 2017/03/15 23:59:50.\n");
+    printf("# Set RTC alarm date/time:   2017/03/15 23:59:55.\n");
+    printf("# Wait system waken-up by RTC alarm interrupt event.\n");
+
+    g_u8IsRTCAlarmINT = 0;
+
+    /* System enter to Power-down */
+    /* To program PWRCTL register, it needs to disable register protection first. */
+    SYS_UnlockReg();
+    printf("\nSystem enter to power-down mode ...\n");
+    /* To check if all the debug messages are finished */
+    while(IsDebugFifoEmpty() == 0);
+    CLK_PowerDown();
+
+    while(g_u8IsRTCAlarmINT == 0);
+
+    /* Read current RTC date/time */
+    RTC_GetDateAndTime(&sReadRTC);
+    printf("System has been waken-up and current date/time is:\n");
+    printf("    %d/%02d/%02d %02d:%02d:%02d\n",
+           sReadRTC.u32Year, sReadRTC.u32Month, sReadRTC.u32Day, sReadRTC.u32Hour, sReadRTC.u32Minute, sReadRTC.u32Second);
+
+
+    printf("\n\n");
+    printf("# Set next RTC alarm date/time: 2017/03/16 00:00:05.\n");
+    printf("# Wait system waken-up by RTC alarm interrupt event.\n");
+    RTC_SetAlarmDate(2017, 03, 16);
+    RTC_SetAlarmTime(0, 0, 5, RTC_CLOCK_24, 0);
+
+    g_u8IsRTCAlarmINT = 0;
+
+    /* System enter to Power-down */
+    /* To program PWRCTL register, it needs to disable register protection first. */
+    SYS_UnlockReg();
+    printf("\nSystem enter to power-down mode ...\n");
+    /* To check if all the debug messages are finished */
+    while(IsDebugFifoEmpty() == 0);
+    CLK_PowerDown();
+
+    while(g_u8IsRTCAlarmINT == 0);
+
+    /* Read current RTC date/time */
+    RTC_GetDateAndTime(&sReadRTC);
+    printf("System has been waken-up and current date/time is:\n");
+    printf("    %d/%02d/%02d %02d:%02d:%02d\n",
+           sReadRTC.u32Year, sReadRTC.u32Month, sReadRTC.u32Day, sReadRTC.u32Hour, sReadRTC.u32Minute, sReadRTC.u32Second);
+
+    while(1)
+    {
+        ;
+    }
+}
+
+/*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
