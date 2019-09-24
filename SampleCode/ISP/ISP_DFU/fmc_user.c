f9e2cb7a6a4c95733444e4e00ddf134c5ef49281
“SPCheng0” <SPCheng0@nuvoton.com>
2019-09-24 17:31:12 +0800

    Add DFU demo code
    Change-Id: I44d1862d6ab62a6e520833d93c16fd6d4327dcf5

---
+++ SampleCode/ISP/ISP_DFU/fmc_user.c
@@ -0,0 +1,77 @@
+/***************************************************************************//**
+ * @file     fmc_user.c
+ * @brief    M031 series FMC driver source file
+ * @version  2.0.0
+ *
+ * @copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "fmc_user.h"
+
+#if 0
+#define ISPCTL      ISPCON
+#define ISPADDR     ISPADR
+#define FMC_ISPCTL_ISPFF_Msk        FMC_ISPCON_ISPFF_Msk
+#endif
+
+int FMC_Proc(unsigned int u32Cmd, unsigned int addr_start, unsigned int addr_end, unsigned int *data)
+{
+    unsigned int u32Addr, Reg;
+
+    for (u32Addr = addr_start; u32Addr < addr_end; data++)
+    {
+        FMC->ISPADDR = u32Addr;
+
+        if((u32Addr& (FMC_FLASH_PAGE_SIZE-1)) == 0 && u32Cmd == FMC_ISPCMD_PROGRAM)
+        {  FMC->ISPCMD = FMC_ISPCMD_PAGE_ERASE;
+           FMC->ISPTRG = 0x1;
+           while (FMC->ISPTRG & 0x1) ; 
+        }
+
+        FMC->ISPCMD = u32Cmd;
+
+        if (u32Cmd == FMC_ISPCMD_PROGRAM)
+        {
+            FMC->ISPDAT = *data;
+        }
+
+        FMC->ISPTRG = 0x1;
+        __ISB();
+
+        while (FMC->ISPTRG & 0x1) ;  /* Wait for ISP command done. */
+
+        Reg = FMC->ISPCTL;
+
+        if (Reg & FMC_ISPCTL_ISPFF_Msk)
+        {
+            FMC->ISPCTL = Reg;
+            return -1;
+        }
+
+        if (u32Cmd == FMC_ISPCMD_READ)
+        {
+            *data = FMC->ISPDAT;
+        }
+
+        if (u32Cmd == FMC_ISPCMD_PAGE_ERASE)
+        {
+            u32Addr += FMC_FLASH_PAGE_SIZE;
+        }
+        else
+        {
+            u32Addr += 4;
+        }
+    }
+
+    return 0;
+}
+
+int ReadData(unsigned int addr_start, unsigned int addr_end, unsigned int *data)    // Read data from flash
+{
+    return FMC_Proc(FMC_ISPCMD_READ, addr_start, addr_end, data);    
+}
+
+int WriteData(unsigned int addr_start, unsigned int addr_end, unsigned int *data)  // Write data into flash
+{
+    return FMC_Proc(FMC_ISPCMD_PROGRAM, addr_start, addr_end, data);
+}
---
+++ SampleCode/ISP/ISP_DFU/main.c
@@ -0,0 +1,88 @@
+/******************************************************************************//**
+ * @file     main.c
+ * @version  V3.00
+ * @brief
+ *           Demonstrate how to upgrade firmware between USB device and PC through USB DFU( Device Firmware Upgrade) class.
+ *           A windows tool is also included in this sample code to connect with USB device.
+ *
+ * @copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "dfu_transfer.h"
+
+#define HCLK_DIV                1
+#define USBD_DIV                1
+
+#define PLL_CLOCK               48000000
+
+#define DetectPin                   PB12
+
+#define V6M_AIRCR_VECTKEY_DATA    0x05FA0000UL
+#define V6M_AIRCR_SYSRESETREQ     0x00000004UL
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+    /* Enable Internal High speed RC oscillator (HIRC) */
+    CLK->PWRCTL |= CLK_PWRCTL_HIRCEN_Msk;
+    /* Waiting for Internal High speed RC clock ready */
+    while ((CLK->STATUS & CLK_STATUS_HIRCSTB_Msk) != CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & ~CLK_CLKSEL0_HCLKSEL_Msk) | CLK_CLKSEL0_HCLKSEL_HIRC ;
+    /* Switch USB clock source to HIRC */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & ~CLK_CLKSEL0_USBDSEL_Msk) | CLK_CLKSEL0_HCLKSEL_HIRC;
+    /* USB Clock = HIRC / 1 */
+    CLK->CLKDIV0 = CLK->CLKDIV0 & ~CLK_CLKDIV0_USBDIV_Msk;
+    /* Enable USB clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_USBDCKEN_Msk ;
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    SystemCoreClockUpdate();
+}
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+
+    /* Unlock write-protected registers */
+    SYS_UnlockReg();
+
+    /* Init system and multi-funcition I/O */
+    SYS_Init();
+
+    /* Enable FMC ISP and APROM update function */
+    FMC->ISPCTL |= (FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_APUEN_Msk | FMC_ISPCTL_ISPFF_Msk);
+
+    if (DetectPin != 0)
+    {
+        goto _APROM;
+    }
+
+    /* Open USB controller */
+    USBD_Open(&gsInfo, DFU_ClassRequest, NULL);
+
+    /*Init Endpoint configuration for DFU */
+    DFU_Init();
+
+    /* Start USB device */
+    USBD_Start();
+
+    /* Enable USB device interrupt */
+    NVIC_EnableIRQ(USBD_IRQn);
+
+    while(1);
+
+_APROM:
+    SYS->RSTSTS = (SYS_RSTSTS_PORF_Msk | SYS_RSTSTS_PINRF_Msk);
+    FMC->ISPCTL &= ~(FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+    
+
+}
