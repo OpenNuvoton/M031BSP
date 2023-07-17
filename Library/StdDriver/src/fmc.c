807714c119b9e22d9ee01f04d3e74a5aa07236e5
MS40 SPCheng0 <SPCheng0@nuvoton.com>
2023-07-17 16:57:38 +0800

    Update FMC Erase comment
    Change-Id: I08fbc7710093553d7b5dfb3293946a86d3b8b2ab

--- Library/StdDriver/src/fmc.c
+++ Library/StdDriver/src/fmc.c
@@ -44,7 +44,7 @@ void FMC_Close(void)
 /**
   * @brief Execute FMC_ISPCMD_PAGE_ERASE command to erase a flash page. The page size is 4096 bytes.
   * @param[in]  u32PageAddr Address of the flash page to be erased.
-  *             It must be a 4096 bytes aligned address.
+  *             It must be a 512 bytes or 2k bytes aligned address.
   * @return ISP page erase success or not.
   * @retval   0  Success
   * @retval   -1  Erase failed
